8201faefa0324b02afd3484ea0de9e50c9a7bc16
weiss21 <waisalirobleh@gmail.com>
2020-02-09 00:53:22 +0000

    reads file

--- ts.cpp
+++ ts.cpp
@@ -11,7 +11,31 @@
 using namespace std;
 int main () {
  
+    string fileName;
+    cout << "Enter filename: "; 
+    cin >> fileName;
     
+    ifstream infile(fileName.c_str());
+    int num;
+    infile >> num; 
+    
+    int dynamarray[num][num];
+    for(int i = 0; i < num; i++)
+    {
+        for(int j = 0; j < num; j++)
+            infile >> dynamarray[i][j];
+    }
+
+    infile.close();
+    
+    cout << "Matrix is: " << endl;
+    for(int i = 0; i < num; i++)
+    {
+        for(int j = 0; j < num; j++){
+            cout << dynamarray[i][j] << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
