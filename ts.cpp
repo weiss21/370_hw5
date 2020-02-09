564bd12f693a02eb94749d0120366278b84976e8
weiss21 <waisalirobleh@gmail.com>
2020-02-09 01:46:05 +0000

    works

--- ts.cpp
+++ ts.cpp
@@ -16,26 +16,59 @@ int main () {
     cin >> fileName;
     
     ifstream infile(fileName.c_str());
-    int num;
-    infile >> num; 
+    int vertices, count, arrows;
+    infile >> vertices; 
     
-    int dynamarray[num][num];
-    for(int i = 0; i < num; i++)
+    int dynamarray[vertices][vertices];
+    for(int i = 0; i < vertices; i++)
     {
-        for(int j = 0; j < num; j++)
+        for(int j = 0; j < vertices; j++)
             infile >> dynamarray[i][j];
     }
 
     infile.close();
     
+    int visited[vertices], check[vertices];
+    for (int i = 0; i < vertices; i++){
+        visited[i] = 0;
+        check[i] = 0;
+    }
+    
+    for (int i = 0; i < vertices; i++){
+        for(int j = 0; j < vertices; j++)
+            visited[i] = visited[i] + dynamarray[j][i];
+    }
+    
+    cout<<"\nTopological Sort: ";
+    arrows = 0;
+    while (count < vertices){
+        for(int k = 0; k < vertices; k++){
+            if(visited[k] == 0 && check[k] == 0){
+                cout<< k <<" ";
+                check[k] = 1;
+                if (arrows < (vertices - 1)){
+                    cout << "--> ";
+                    arrows++;
+                }
+            }
+            
+            for(int p = 0; p < vertices; p++){
+                if(dynamarray[p][k] == 1)
+                    visited[k]--;
+            }
+        }
+        count++;
+    }
+    cout << endl;
+    /*
     cout << "Matrix is: " << endl;
-    for(int i = 0; i < num; i++)
+    for(int i = 0; i < vertices; i++)
     {
-        for(int j = 0; j < num; j++){
+        for(int j = 0; j < vertices; j++){
             cout << dynamarray[i][j] << " ";
         }
         cout << endl;
     }
-
+    */
     return 0;
 }
